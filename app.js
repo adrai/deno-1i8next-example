1c147bc3aa504ad5bef19464a89b1e982e32092b
Adriano Raiano <adriano@raiano.ch>
2020-04-15 15:10:49 +0200

    more readme

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { Application } from 'https://deno.land/x/abc/mod.ts'
 
 // i18next in action...
 import i18next from 'https://deno.land/x/i18next/index.js'
+// import i18next from 'https://raw.githubusercontent.com/i18next/i18next/master/src/index.js'
 // import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js'
 // import i18next from '../i18next/src/index.js'
 import HttpBackend from 'https://cdn.jsdelivr.net/gh/i18next/i18next-http-backend/index.js'
