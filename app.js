d39ff5623e5e7c833c4009d290a487cc6a198f6f
Adriano Raiano <adriano@raiano.ch>
2020-04-15 15:13:06 +0200

    different import

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ import { Application } from 'https://deno.land/x/abc/mod.ts'
   .start({ port: 8080 })
 
 // i18next in action...
-import i18next from 'https://deno.land/x/i18next/index.js'
-// import i18next from 'https://raw.githubusercontent.com/i18next/i18next/master/src/index.js'
+// import i18next from 'https://deno.land/x/i18next/index.js'
+import i18next from 'https://raw.githubusercontent.com/i18next/i18next/master/src/index.js'
 // import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js'
-// import i18next from '../i18next/src/index.js'
-import HttpBackend from 'https://cdn.jsdelivr.net/gh/i18next/i18next-http-backend/index.js'
+import HttpBackend from 'https://raw.githubusercontent.com/i18next/i18next-http-backend/master/index.js'
+// import HttpBackend from 'https://cdn.jsdelivr.net/gh/i18next/i18next-http-backend/index.js'
 i18next.use(HttpBackend).init({
   lng: 'en',
   fallbackLng: 'en',
