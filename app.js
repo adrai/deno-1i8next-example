a82b58a45c78b3421fbe3ef3391d5f6da323a1e2
Adriano Raiano <adriano@raiano.ch>
2020-04-15 14:58:20 +0200

    some comments

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+// serve translations
 import { Application } from 'https://deno.land/x/abc/mod.ts'
-
-const app = new Application()
-app
+(new Application())
   .static('/locales', './locales')
   .start({ port: 8080 })
 
-
+// i18next in action...
 import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js'
 // import i18next from '../i18next/src/index.js'
 import HttpBackend from 'https://cdn.jsdelivr.net/gh/i18next/i18next-http-backend/index.js'
-
 i18next.use(HttpBackend).init({
   lng: 'en',
   fallbackLng: 'en',
